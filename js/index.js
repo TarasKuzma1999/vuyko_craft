357ae4ff449f684a8218c43524d63e035c60c85c
TarasKuzma1999 <56336985+TarasKuzma1999@users.noreply.github.com>
2020-10-05 19:26:47 +0300

    added questions and our brewery

--- js/index.js
+++ js/index.js
@@ -3,34 +3,50 @@ $('.beer_carousel').slick({
     autoplaySpeed: 5000,
 });
 
-// $('.choose_tour').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 5000,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     responsive: [{
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 1,
-//                 infinite: true,
-//                 dots: true
-//             }
-//         },
-//         {
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         },
-
-//     ]
-// });
+$('.choose_tour').slick({
+    dots: true,
+    infinite: false,
+    speed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1160,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 990,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+});
 
 $('.responce_carousel').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     dots: true
+});
+
+
+$('.our_brewery_carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.our_brewery_carousel_nav'
+});
+$('.our_brewery_carousel_nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.our_brewery_carousel',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
 });
