20c5372a2b5e896f985ce6f2be9bd5fffdce5483
TarasKuzma1999 <56336985+TarasKuzma1999@users.noreply.github.com>
2020-10-06 17:45:23 +0300

    fixed errors

--- js/index.js
+++ js/index.js
@@ -3,37 +3,11 @@ $('.beer_carousel').slick({
     autoplaySpeed: 5000,
 });
 
-$('.choose_tour').slick({
-    dots: true,
-    infinite: false,
-    speed: 5000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 1160,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 990,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-    ]
-});
-
 $('.responce_carousel').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     dots: true
 });
-
 
 $('.our_brewery_carousel').slick({
     slidesToShow: 1,
